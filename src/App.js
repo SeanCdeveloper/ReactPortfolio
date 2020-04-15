10813aee934cb19c9bd82286856d6d414b358561
Your Name <you@example.com>
2020-04-15 17:17:18 -0400

    pages connected to React Router.

--- src/App.js
+++ src/App.js
@@ -1,19 +1,31 @@
 import React from 'react';
-import './App.css';
+//import './App.css';
+import {BrowserRouter as Router, Route } from 'react-router-dom';
 import Nav from './components/Nav/Nav'
 import Header from './components/Header/Header'
 import Project from './components/Project/Project'
 import Footer from './components/Footer/Footer'
-
+import NavTabs from './components/NavTabs/NavTabs'
+import Home from './components/pages/Home';
+import About from './components/pages/About';
+import Portfolio from './components/pages/Portfolio';
+import Contact from './components/pages/Contact';
 
 function App() {
   return (
+    <Router>
     <div className="App">
-      <Nav />
-     <Header />
+      {/* <Nav /> */}
+      <NavTabs />
+     {/* <Header />
      <Project />
-     <Footer />
+     <Footer /> */}
+    <Route exact path="/" component={Home}/> 
+    <Route exact path="/about" component={About}/>
+    <Route exact path="/portfolio" component={Portfolio}/>
+    <Route exact path="/contact" component={Contact}/>
     </div>
+    </Router>
   );
 }
 
---
+++ src/components/pages/Learn.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const Learn = () => {
+    return (
+        <div>
+        <p>Learn Page</p>
+        </div>
+    )
+}
+
+export default Learn;
