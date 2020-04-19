d0938bf2781241c1092cf439e84e2099daded09f
Your Name <you@example.com>
2020-04-18 22:57:48 -0400

    Final Push.

--- src/components/FancyJumbotron/FancyJumbotron.js
+++ src/components/FancyJumbotron/FancyJumbotron.js
@@ -1,21 +1,22 @@
 import React from 'react';
 import './FancyJumbotron.css'
-import { Link} from 'react-router-dom';
+import { Link } from 'react-router-dom';
 
 const FancyJumbotron = () => {
   return (
     <div className="text-white fancy-jumbo text-center py-5 px-4">
-      <div className="jumbotron cardImg1 card card-image" style={{background: "black"}}>
+      <div className="jumbotron cardImg1 card card-image" style={{ background: "black" }}>
         <div>
+        </div>
+        <div>
+          <h2 className="card-title h1-responsive pt-3 mb-5 font-bold"><strong>Sean Cumming Coding Page</strong></h2>
+          <p className="mx-5 mb-5"></p>
+          <Link to="/portfolio" role="button" className="btn btn1 btn-link"><div className="text-center">
+            <button style={{ height: "4em", fontSize: "20px" }} type="button" className="btn btn-outline-red btn-red">View Portfolio</button></div></Link>
+            <div style={{height: "55px"}}></div>
+        </div>
       </div>
-    <div>
-      <h2 className="card-title h1-responsive pt-3 mb-5 font-bold"><strong>Sean Cumming's Coding Page</strong></h2>
-      <p className="mx-5 mb-5"></p>
-      <Link to="/portfolio" role="button" className="btn btn1 btn-link"><div className="text-center">
-      <button style={{height: "4em", fontSize: "20px"}} type="button" className="btn btn-outline-red btn-red">View Portfolio</button></div></Link>
-    </div>
-  </div>
-</div>
+    </div>  
   )
 }
 
--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -7,7 +7,7 @@ const Footer = () => {
         <div>
             <footer style={{background: 'black'}} className="page-footer">
             <div className="footer-copyright text-center py-3">© 2020 Copyright:
-              <a href="#">&nbsp;SCDeveloper</a>
+              <a href="https://github.com/SeanCdeveloper">&nbsp;SCDeveloper</a>
             </div>
             </footer>
         </div>
